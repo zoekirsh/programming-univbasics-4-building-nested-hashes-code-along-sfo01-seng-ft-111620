d9d23a74a28befb5713d1a5b2a3f18959a17a978
maxwellbenton <maxwellbenton@gmail.com>
2020-03-02 16:32:23 -0500

    update require_relative file locaions

--- spec/01_challenge_spec.rb
+++ spec/01_challenge_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-require_relative '../lib/first_challenge.rb'
+require_relative '../lib/01_first_challenge.rb'
 
 describe "first_challenge" do
 	it "sets a variable, epic_tragedy, equal to a hash containing keys of family names and values of empty hashes" do
--- spec/03_challenge_spec.rb
+++ spec/03_challenge_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-require_relative '../lib/third_challenge.rb'
+require_relative '../lib/03_third_challenge.rb'
 
 describe "third_challenge" do
 	it "fills out the Montague character role keys with the key/value pairs describing their attributes" do
