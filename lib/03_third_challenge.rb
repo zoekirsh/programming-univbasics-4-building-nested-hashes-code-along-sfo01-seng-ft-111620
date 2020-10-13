41953a8f27a37a86753b0df499ef4b3f159359a9
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-13 23:42:36 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,17 +14,21 @@ def third_challenge
         :age => "15", 
         :status => "alive"
         },
-      :hero_friends => ["Mercutio", ""]
+      :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet", 
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
